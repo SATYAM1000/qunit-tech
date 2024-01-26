27ca73a0830dc8747a45cdd31b684e2f9e6daf82
Yash Chopra <chopray567@gmail.com>
2024-01-26 13:37:19 +0530

    respnsive bug

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -136,12 +136,13 @@ const Page = () => {
                                     </div>
                                     <div className="flex items-center me-4">
                                         <input id="inline-2-radio" type="radio" value="Profession" name="category" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                                        <label htmlFor="category" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Profession</label>
+                                        <label htmlFor="category" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Profession/Developer</label>
                                     </div>
                                     <div className="flex items-center me-4">
-                                        <input id="inline-2-radio" type="radio" value="Developer" name="category" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                                        <label htmlFor="category" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Developer</label>
+                                        <input id="inline-2-radio" type="radio" value="Freelancer" name="category" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                        <label htmlFor="category" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Freelancer</label>
                                     </div>
+                                    
                                 </div>
                                 <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                     onClick={HandleSumitForRegister}
